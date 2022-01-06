43c979704891c8839293d7e233d63dea3c392bab
devkhaldi <dev.khaldi@gmail.com>
2022-01-06 01:32:54 +0000

    fix color and best offers duration

--- client/src/Home/BestOffers.js
+++ client/src/Home/BestOffers.js
@@ -1,59 +1,16 @@
-import React, { useState } from 'react'
-import australia from '../img/australia.jpg'
-import italy from '../img/italy.jpg'
-import japan from '../img/japan.jpg'
-import spain from '../img/spain.jpg'
-import turkey from '../img/turkey.jpg'
-import uk from '../img/uk.jpg'
-import Offer from './Offer'
-import './css/BestOffers.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react"
+import australia from "../img/australia.jpg"
+import italy from "../img/italy.jpg"
+import japan from "../img/japan.jpg"
+import spain from "../img/spain.jpg"
+import turkey from "../img/turkey.jpg"
+import uk from "../img/uk.jpg"
+import Offer from "./Offer"
+import "./css/BestOffers.css"
+import { Link } from "react-router-dom"
 
 const BestOffers = () => {
-  const [BestOffers, setBestOffers] = useState([
-    {
-      id: 1,
-      image: australia,
-      country: 'Australia',
-      price: 11200,
-      cities: ['Sydney', 'Brisbane', 'Melbourne']
-    },
-    {
-      id: 2,
-      image: italy,
-      price: 5500,
-      country: 'Italy',
-      cities: ['Milan', 'Rome', 'Turin']
-    },
-    {
-      id: 3,
-      image: japan,
-      price: 8900,
-      country: 'Japan',
-      cities: ['Tokyo', 'Kyoto']
-    },
-    {
-      id: 4,
-      image: spain,
-      price: 3000,
-      country: 'Spain',
-      cities: ['Barcelone', 'Madrid']
-    },
-    {
-      id: 5,
-      image: uk,
-      price: 9250,
-      country: 'United Kingdom',
-      cities: ['London', 'Monchester', 'Liverpoul', 'Glasgow']
-    },
-    {
-      id: 6,
-      image: turkey,
-      price: 4500,
-      country: 'Turkey',
-      cities: ['Istanboul', 'Ankara']
-    }
-  ])
+  const [BestOffers, setBestOffers] = useState(bestOffersData)
   return (
     <div className='best-offers-container'>
       <div className='title'>
@@ -71,5 +28,56 @@ const BestOffers = () => {
     </div>
   )
 }
+
+let bestOffersData = [
+  {
+    id: 1,
+    image: australia,
+    country: "Australia",
+    price: 11200,
+    cities: ["Sydney", "Brisbane", "Melbourne"],
+    duration: 15,
+  },
+  {
+    id: 2,
+    image: italy,
+    price: 5500,
+    country: "Italy",
+    cities: ["Milan", "Rome", "Turin"],
+    duration: 9,
+  },
+  {
+    id: 3,
+    image: japan,
+    price: 8900,
+    country: "Japan",
+    cities: ["Tokyo", "Kyoto"],
+    duration: 10,
+  },
+  {
+    id: 4,
+    image: spain,
+    price: 3000,
+    country: "Spain",
+    cities: ["Barcelone", "Madrid"],
+    duration: 5,
+  },
+  {
+    id: 5,
+    image: uk,
+    price: 9250,
+    country: "United Kingdom",
+    cities: ["London", "Monchester", "Glasgow"],
+    duration: 10,
+  },
+  {
+    id: 6,
+    image: turkey,
+    price: 4500,
+    country: "Turkey",
+    cities: ["Istanboul", "Ankara"],
+    duration: 5,
+  },
+]
 
 export default BestOffers
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react'
-import Navbar from '../Component/Navbar'
-import Footer from '../Component/Footer'
-import Hero from './Hero'
-import BestOffers from './BestOffers'
-import WhyMenaraTravels from './WhyMenaraTravels'
-import CustomerSayings from './CustomerSayings'
-import FindTour from './FindTour'
+import React, { useEffect } from "react"
+import Navbar from "../Component/Navbar"
+import Footer from "../Component/Footer"
+import Hero from "./Hero"
+import BestOffers from "./BestOffers"
+import WhyMenaraTravels from "./WhyMenaraTravels"
+import CustomerSayings from "./CustomerSayings"
+import FindTour from "./FindTour"
 
 const Home = () => {
   useEffect(() => {
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     })
   }, [])
+
   return (
     <React.Fragment>
       <Navbar />
